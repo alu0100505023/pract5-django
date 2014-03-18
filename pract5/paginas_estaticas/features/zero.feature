e0ebe00195ee408c72138626955490df518bf8c1
Javier CRG <alu0100505023@ull.edu.es>
2014-03-18 10:29:34 +0000

    resolviendo error home

---
+++ pract5/paginas_estaticas/features/zero.feature
@@ -0,0 +1,5 @@
+Feature: Web
+
+	Scenario: Text inside
+	Given I access the url "/home"
+	#Then I see the header "Home"
--- pract5/pract5/settings.py
+++ pract5/pract5/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	 'paginas_estaticas'
+	 'paginas_estaticas',
+	'lettuce.django'
 )
 
 MIDDLEWARE_CLASSES = (
